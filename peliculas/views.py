9a79c4a369a8184022000890301e8f635e9c2800
josehermosillaa <jose.hermosillaa@usach.cl>
2023-08-09 19:51:05 -0400

    se actualiza la vista de carga de datos adecuando a la nueva api

--- peliculas/views.py
+++ peliculas/views.py
@@ -10,20 +10,20 @@ def index_view(request):
     return render(request, "peliculas/index.html", context)
 
 def load_view(request):
-    url = "https://studio-ghibli-films-api.herokuapp.com/api"
+    url = "https://ghibliapi.vercel.app/films"
     response = requests.get(url)
 
     if response.status_code == 200:
         # data = response.json()
         # keys = response.json().keys()
-        values = response.json().values()
+        values = response.json()
         for value in values:
             Pelicula.objects.update_or_create(
                 title=value["title"],
-                poster=value["poster"],
-                genre=value["genre"],
-                rating=value["rating"],
-                release=datetime.datetime.strptime(value["release"],"%B %d, %Y") if value["release"] !="TBA" else None,
+                poster=value["image"],
+                genre=value["original_title"],
+                rating=value["rt_score"],
+                release=datetime.datetime.strptime(value["release_date"],"%Y") if value["release_date"] !="TBA" else None,
                 director=value["director"]
             )
         return HttpResponse("<h1>los datos se cargaron correctamente</h1>")
